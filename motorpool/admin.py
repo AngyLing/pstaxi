c8ed0674fd20b9f3a430871e158ad55dcc036a34
Angy <work220@bk.ru>
2021-06-27 02:36:41 +0300

    Add classes Option, Passport, Profile

--- motorpool/admin.py
+++ motorpool/admin.py
@@ -7,3 +7,23 @@ from motorpool import models
 @admin.register(models.Brand)
 class BrandAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(models.Option)
+class OptionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Auto)
+class AutoAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.VehiclePassport)
+class VehiclePassportAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    pass
--- motorpool/models.py
+++ motorpool/models.py
@@ -12,3 +12,62 @@ class Brand(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Option(models.Model):
+    title = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = 'Опции'
+
+    def __str__(self):
+        return self.title
+
+
+class Auto(models.Model):
+    AUTO_CLASS_ECONOMY = 'e'
+    AUTO_CLASS_COMFORT = 'c'
+    AUTO_CLASS_BUSINESS = 'b'
+
+    AUTO_CLASS_CHOICES = (
+        (AUTO_CLASS_ECONOMY, 'economy'),
+        (AUTO_CLASS_COMFORT, 'comfort'),
+        (AUTO_CLASS_BUSINESS, 'business'),
+    )
+    number = models.CharField(max_length=15)
+    description = models.TextField(max_length=500, default='', blank=True)
+    year = models.PositiveSmallIntegerField(null=True)
+    auto_class = models.CharField(max_length=1, null=True, choices=AUTO_CLASS_CHOICES, default=AUTO_CLASS_ECONOMY)
+    brand = models.ForeignKey(Brand, null=True, on_delete=models.CASCADE)
+    options = models.ManyToManyField(Option)
+
+    def __str__(self):
+        return self.number
+
+    class Meta:
+        verbose_name_plural = 'Автомобили'
+        verbose_name = 'Автомобиль'
+
+
+class VehiclePassport(models.Model):
+    auto = models.OneToOneField(Auto, on_delete=models.CASCADE)
+    vin = models.CharField(max_length=30, verbose_name='Идентификационный номер (VIN)')
+    engine_volume = models.SmallIntegerField(verbose_name='Объем двигателя, куб.см')
+    engine_power = models.SmallIntegerField(verbose_name='Мощность двигателя, л.с.')
+
+    def __str__(self):
+        return f'{self.auto}::{self.vin}'
+
+    class Meta:
+        verbose_name_plural = 'Паспорта машин'
+        verbose_name = 'Паспорт машины'
+
+
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    description = models.TextField(max_length=1000, null=True, blank=True, verbose_name='О себе')
+    phone = models.CharField(max_length=30, null=True, blank=True, verbose_name='Телефон')
+    address = models.CharField(max_length=250, null=True, blank=True, verbose_name='Адрес')
